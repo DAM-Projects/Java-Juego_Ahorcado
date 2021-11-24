0d833854f60540c9610f305f7ed8f6452249ad3e
jkutkut <jorgeregonzalez@gmail.com>
2021-11-24 09:17:01 +0100

    Interfaz class added

--- src/damProjects/juegoAhorcado/JuegoAhorcadoInterfaz.java
+++
@@ -1,7 +0,0 @@
-package damProjects.juegoAhorcado;
-
-public class JuegoAhorcadoInterfaz {
-	public static void main() {
-		System.out.println("Hello world!");
-	}
-}
---
+++ src/damProjects/juegoAhorcado/interfaz/JuegoAhorcadoInterfaz.java
@@ -0,0 +1,9 @@
+package damProjects.juegoAhorcado.interfaz;
+
+public class JuegoAhorcadoInterfaz {
+
+	public static void main(String[] args) {
+		System.out.println("Holaaa");
+
+	}
+}
